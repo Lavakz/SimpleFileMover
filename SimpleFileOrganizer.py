fbfc671c3503b222deac3209002e695f5b8a33fc
aquarock <aqua.rock.slug@gmail.com>
2024-03-08 15:30:46 -0600

    rename

--- SimpleFileOrganizer.py
+++ SimpleFileOrganizer.py
@@ -5,20 +5,25 @@ import shutil
 import sys
 
 dir_filename = "dirs.txt"
+file_types = (".jpg", ".png")
 files = {}
 fav_dirs = []
 batch_move = False
 
 windows = False
-image_viewer = "feh"
+linux_image_viewer = "chafa"
 
 
 class Random_File:
-    def __init__(self, dir=""):
+    def __init__(self, file_dir=""):
         self.dir: str = (
-            random.choice(list(files.keys())) if dir == "" else fav_dirs[dir]
+            random.choice(list(files.keys())) if file_dir == "" else fav_dirs[file_dir]
         )
-        self.file: str = random.choice(os.listdir(self.dir))
+        files_in_dir = os.listdir(self.dir)
+        if not files_in_dir:
+            print("No more files to move...")
+            return
+        self.file: str = random.choice(files_in_dir)
         self.path: str = f"{self.dir}/{self.file}"
 
     def rate(self, i=0, count=1):
@@ -30,12 +35,16 @@ class Random_File:
             return
         print(f"\n\b[ {i+1} / {count} ]")
         self.open()
-        feedback: str = input(f"Rating (1-{len(fav_dirs)}): ")
-        rating: int = int(-1 if feedback == "" or not feedback.isdigit() else feedback)
-        if 0 < rating and rating < len(fav_dirs) - 1:
-            new_path: str = fav_dirs[rating - 1]
+        for fav_i, fav_dir in enumerate(fav_dirs):
+            print(f"{fav_i+1}) {fav_dir}")
+        feedback: str = input(f"Move to... (1-{len(fav_dirs)}): ")
+        rating: int = (
+            int(-1 if feedback == "" or not feedback.isdigit() else feedback)
+        ) - 1
+        if 0 <= rating and rating < len(fav_dirs):
+            new_path: str = fav_dirs[rating]
         else:
-            new_path: str = "no rating"
+            new_path: str = "not moved"
 
         if batch_move:
             Random_File().rate(i + 1, count)
@@ -48,24 +57,24 @@ class Random_File:
         if windows:
             os.startfile(self.path)
         else:
-            subprocess.call([image_viewer, self.path])
-        print(f"Opened {self.file} in\n{self.dir}\n")
+            subprocess.call([linux_image_viewer, self.path])
+        print(f"{self.file} in {self.dir}")
         return
 
     def move(self, new_path):
-        if new_path == "no rating":
-            print("No rating given")
+        if new_path == "not moved":
+            print("file not moved")
             return
         shutil.move(self.path, new_path)
         print(f"Moved {self.file} to {new_path}")
 
 
-def get_files(dir, overwrite=False):
-    if len(files[dir]) == 0 or overwrite:  # scan dir if not already scanned
-        files[dir] = [
-            file for file in os.listdir(dir) if file.endswith((".mp4", ".mkv", ".webm"))
+def get_files(file_dir, overwrite=False):
+    if len(files[file_dir]) == 0 or overwrite:  # scan dir if not already scanned
+        files[file_dir] = [
+            file for file in os.listdir(file_dir) if file.endswith(file_types)
         ]
-    return files[dir]
+    return files[file_dir]
 
 
 def get_input(prompt="-> "):
@@ -78,12 +87,12 @@ def get_input(prompt="-> "):
 def get_info():
     for d, f in files.items():  # scan all dirs
         f = get_files(d, True)
-    print("\nUnrated Folders:")
-    for i, dir in enumerate(list(files.keys())):
-        print(f"{i+1}) {len(get_files(dir))} files in {dir}")
-    print("\nRated Folders:")
-    for i, dir in enumerate(fav_dirs):
-        print(f"{i}) {len(os.listdir(dir))} files in {dir}")
+    print("\nUnorganized Folders:")
+    for i, file_dir in enumerate(list(files.keys())):
+        print(f"{i+1}) {len(get_files(file_dir))} files in {file_dir}")
+    print("\nOrganized Folders:")
+    for i, file_dir in enumerate(fav_dirs):
+        print(f"{i}) {len(os.listdir(file_dir))} files in {file_dir}")
 
 
 def get_dirs():
@@ -91,12 +100,12 @@ def get_dirs():
         dir_names = [
             line.rstrip() for line in file.readlines() if not line.startswith("#")
         ]
-        for i in range(dir_names.index("unrated:") + 1, len(dir_names)):
+        for i in range(dir_names.index("unorganized:") + 1, len(dir_names)):
             if dir_names[i] != "":
                 files[dir_names[i]] = []
             else:
                 break
-        for i in range(dir_names.index("rated:") + 1, len(dir_names)):
+        for i in range(dir_names.index("organized:") + 1, len(dir_names)):
             if dir_names[i] != "":
                 fav_dirs.append(dir_names[i])
             else:
@@ -110,9 +119,9 @@ def main():
 
     print("\n\bMain Menu:")
     menu = [
-        "Rate Files",
-        "Open Random Rated File",
-        "Open Random Unrated File",
+        "Move Files",
+        "Open Random Organized File",
+        "Open Random Unorganized File",
         "Get Info",
         "Quit",
     ]
@@ -122,15 +131,15 @@ def main():
     match get_input():
         case 1:
             if len(fav_dirs) == 0:
-                print("No favorite directories found")
+                print("No organized directories found")
             else:
-                count = input("\nHow many files to rate?\n-> ")
+                count = input("\nHow many files to move?\n-> ")
                 if count == "":
                     count = "1"
                 if count.isdigit() == True:
                     Random_File().rate(0, int(count if int(count) > 0 else 1))
         case 2:
-            rating = int(input("\nWhat rating?\n-> "))
+            rating = int(input("\nWhich directory?\n-> "))
             Random_File(rating).open()
             while input(f"Open another file in {fav_dirs[rating]}? (y/n)\n-> ") != "n":
                 Random_File(rating).open()
